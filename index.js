0404f08a9f53af7fa69d97211ec2fc5364e3867f
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-07-08 15:22:22 +0900

    ADD : rebuild

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var menu = "korean";
 var menu_btn = document.getElementsByClassName("menu_btn");
 var game_tab = document.getElementById("game");
 var game_tab2 = document.getElementsByTagName("body");
-change_menu_tab();
-
 const input = document.querySelector('#answer');
 const scoreDisplay = document.querySelector('.score');
 const timeDisplay = document.querySelector('.time');
@@ -12,7 +9,7 @@ let words = [];
 
 let score = 0;
 let isPlaying = false;
-const GAME_TIME = 9;
+const GAME_TIME = 7;
 let currentTime;
 
 
@@ -23,6 +20,7 @@ function showWord() {
 for(var i=0;i<menu_btn.length;i++){
     menu_btn[i].addEventListener("click",change_menu_value);
 }
+
 fetch('https://random-word-api.herokuapp.com/word?number=100').then((res) => {
   res.json().then((data) => {
     data.forEach((word) => {
@@ -30,51 +28,43 @@ fetch('https://random-word-api.herokuapp.com/word?number=100').then((res) => {
     });
   });
 });
-function change_menu_value(){
-    menu=this.value;
-    console.log(menu);
-    change_menu_tab();
-}
 
-function change_menu_tab(){
-    if(menu=="korean"){
-        document.body.style.backgroundColor="#FFD6A5";
-    }else if(menu=="english"){
-        document.body.style.backgroundColor="#FDFFB6";
+wordDisplay.addEventListener('click',setGame);
+
+function setGame(){
+    if(!isPlaying){
+        currentTime = GAME_TIME;
+        score=0;
+        isPlaying = true;
+        showWord();
+        remaining();
+        isPlayingGame();
+        isEndingG();
     }
-}
-
-var ans="Start!";
-
-function getEnter(e){
-    if(e.keyCode==13){
-        if(document.getElementById("answer").value==ans){
-            if(ans=="Start!"){
-                currentTime = GAME_TIME;
-                isPlaying = true;
-                showWord();
-                remainingTime();
-                isPlayingGame();
-            }
-        }
-        document.getElementById("answer").value="";
-    }
+    
 }
 
 function startGame(){
     console.log("start Game");
 }
 
-
-
-
+var rem
+function remaining(){
+    rem = setInterval(remainingTime,1000);
+}
 function remainingTime() {
-    setInterval(() => {
-        wordMatch(); // 정답이 맞는지 계속 확인
-  
-        timeDisplay.innerText = currentTime;
-        timeDisplay.innerText > 0 ? currentTime-- : false;
-    }, 1000);
+    wordMatch();
+    timeDisplay.innerText = currentTime;
+    timeDisplay.innerText > 0 ? currentTime-- : false;
+}
+var end;
+function isEndingG(){
+    end = setInterval(isEndingGame,100);
+}   
+function isEndingGame(){
+    if((currentTime==0||score==10)){
+        endGame();
+    }
 }
 function isPlayingGame() {
     if (isPlaying) {
@@ -83,14 +73,23 @@ function isPlayingGame() {
 }
 function wordMatch() {
     input.addEventListener('input', () => {
-      if (isPlaying) {
-        if (wordDisplay.innerText.toLowerCase() === input.value.toLowerCase()) {
-          score += 1;
-          scoreDisplay.innerText = score;
-          input.value = '';
-          currentTime = GAME_TIME;
-          showWord();
+        if (isPlaying) {
+            if (wordDisplay.innerText.toLowerCase() === input.value.toLowerCase()) {
+                score += 1;
+                scoreDisplay.innerText = score;
+                input.value = '';
+                currentTime += GAME_TIME;
+                showWord();
+            }
         }
-      }
     });
-  }
+}
+function endGame(){
+    isPlaying=false;
+    var res = (score*10)+(currentTime*2);
+    wordDisplay.innerText = "Restart?\n"+"Score : "+res;
+    remainingTime();
+    clearInterval(end);
+    clearInterval(rem);
+    wordDisplay.addEventListener('click',setGame);
+}
